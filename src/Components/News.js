753cb8718d45af7176b01ae52beb6ebc24a4a673
GaneshNeupane01 <ganeshneupane1023@gmail.com>
2024-05-24 22:12:15 +0545

    fc

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,9 +4,17 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        abc
+      <div className='container my-4'>
+       <h1>UpdateToday - Top Headlines</h1>
+       <div className="row">
+        <div className="col-md-4">
         <NewsItem/>
+        </div>
+        <div className="col-md-4">
+        <NewsItem title="mytitle" description = "mydesc"/>
+        </div>
+        
+        </div>
       </div>
     )
   }
